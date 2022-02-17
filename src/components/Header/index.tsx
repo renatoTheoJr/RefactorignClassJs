fe0edd9f94b60934872e0b6ff5f757c027e7b9c9
Renato Theodoro <renatotheojunior@gmail.com>
2022-02-17 16:17:30 -0300

    Header and Food Components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,37 @@
-import { Component } from 'react';
 import { FiPlusSquare } from 'react-icons/fi';
 
 import { Container } from './styles';
 import Logo from '../../assets/logo.svg';
+
+interface HeaderProps {
+  openModal: () => void;
+}
+
+export default function Header({openModal} : HeaderProps){
+
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button
+              type="button"
+              onClick={openModal}
+            >
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  )
+}
+
+/*
 
 class Header extends Component {
   render() {
@@ -14,7 +43,7 @@ class Header extends Component {
           <img src={Logo} alt="GoRestaurant" />
           <nav>
             <div>
-              <button
+              <buttons
                 type="button"
                 onClick={openModal}
               >
@@ -32,3 +61,4 @@ class Header extends Component {
 };
 
 export default Header;
+*/
